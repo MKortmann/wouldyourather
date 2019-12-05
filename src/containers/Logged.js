2c57fd9078bb86ff1e76242d1329d830355036b0
MKortmann <companymvms@gmail.com>
2019-12-05 18:26:56 +0100

    add: load answered and unanswered questions

--- src/containers/Logged.js
+++ src/containers/Logged.js
@@ -4,46 +4,68 @@ import Questions from "./Questions";
 import Button from "../components/Button";
 import { _getUsers, _getQuestions, _saveQuestion, _saveQuestionAnswer } from "../_DATA";
 
-
 const Logged = (props) => {
 
-  const [unanswered, setUnanswered] = useState([]);
-  const [answeredIds, setAnsweredIds] = useState([]);
+  const [ answeredQuestionsShowFlag, setAnsweredQuestionsShowFlag ] = useState(false);
+
+  const [answeredQuestions, setAnsweredQuestions] = useState([]);
+  const [unansweredQuestions, setUnansweredQuestions] = useState([]);
+
+  const [questions, setQuestions] = useState(null);
+  const [users, setUsers] = useState(null);
 
 
   // here I want to load the questions and users
   useEffect( () => {
+    const questionsAnsweredTemp = [];
+    const questionsUnAnsweredTemp = [];
+
     _getUsers()
       .then(res => {
-
-        // to avoid errors if the user is not authenticated... We must remove this later on!
-        if(localStorage.getItem("authUser")) {
-          console.log(res[JSON.parse(localStorage.getItem("authUser"))].questions)
-          setAnsweredIds(res[JSON.parse(localStorage.getItem("authUser"))].questions)
-        }
-
+        setUsers(res);
       })
 
     _getQuestions()
       .then(res => {
-        answeredIds.forEach( item => {
-          console.log(res[item]);
+        setQuestions(res);
+        const arrayQuestions = Object.values(res);
+        arrayQuestions.forEach( (item, index) => {
+          // check if the author of this questions is the logged authors
+          if ( arrayQuestions[index].author ===  JSON.parse(localStorage.getItem("authUser")) ) {
+            questionsAnsweredTemp.push(item);
+          } else {
+            questionsUnAnsweredTemp.push(item);
+          }
         })
-      })
 
-  })
+        setAnsweredQuestions(questionsAnsweredTemp);
+        setUnansweredQuestions(questionsUnAnsweredTemp);
+        console.log(`Questions Answered: ${questionsAnsweredTemp}`);
+        console.log(`Questions Unanswered: ${questionsUnAnsweredTemp}`);
 
+      });
 
+  }, [])
+
+  const handleClickShowFlag = () => {
+    setAnsweredQuestionsShowFlag(false);
+    alert("handleClickShowFlag")
+  }
+  const handleClickNotShowFlag = () => {
+    setAnsweredQuestionsShowFlag(true);
+    alert("clicked")
+  }
 
   return (
     <div>
       <br />
-      <Button label={"unanswered"} bgColor={"rgba(0,212,255,0.6)"}/>
-      <Button label={"answered"}  color={"white"}/>
-      <Questions />
+      <Button clicked={handleClickShowFlag} label={"unanswered"} bgColor={"rgba(0,212,255,0.6)"}/>
+      <Button clicked={handleClickNotShowFlag} label={"answered"}  color={"white"}/>
+      <Questions answeredQuestions={answeredQuestions}
+        answeredQuestionsShowFlag={answeredQuestionsShowFlag}
+        unansweredQuestions={unansweredQuestions}/>
     </div>
   )
 }
-
 
 export default withRouter(Logged);
--- src/containers/Questions.js
+++ src/containers/Questions.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
@@ -19,22 +19,43 @@ const useStyles = makeStyles({
   }
 });
 
+export default function Questions(props) {
 
 
 
-export default function Questions() {
+  useEffect( () => {
+
+    console.log(`[Questions]: ${props.answeredQuestions}`);
+    console.log(`[Not Answered Questions]: ${props.unansweredQuestions}`);
+
+  }, [])
+
+
+  let componentQuestions = null;
+  if (props.answeredQuestionsShowFlag === true) {
+    componentQuestions = (
+        props.answeredQuestions.map((item, index) => {
+          return(<Question />)
+        })
+    )
+  } else {
+    componentQuestions = (
+        props.unansweredQuestions.map((item, index) => {
+          return(<Question />)
+        })
+    )
+  }
+
   const classes = useStyles();
 
   return (
     <React.Fragment>
       <CssBaseline />
       <Container >
+      <div className={classes.root} component="div" style={{ backgroundColor: '#cfe8fc', height: '100vh'}}>
+      {componentQuestions}
 
-        <div className={classes.root} component="div" style={{ backgroundColor: '#cfe8fc', height: '100vh'}}>
-          <Question />
-          <Question />
-          <Question />
-        </div>
+      </div>
       </Container>
     </React.Fragment>
   );
