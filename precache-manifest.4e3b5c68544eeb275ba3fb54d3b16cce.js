021f4b98da496d828685426fb35ccd92173fbabf
MKortmann <companymvms@gmail.com>
2020-02-20 21:25:47 +0100

    Updates

---
+++ precache-manifest.4e3b5c68544eeb275ba3fb54d3b16cce.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d22e3705e742674420aca382a312bd4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fe52c5ef97c7fd1c23d8",
+    "url": "/static/css/main.1779617f.chunk.css"
+  },
+  {
+    "revision": "a386046b184d00032ec9",
+    "url": "/static/js/2.9a5d554f.chunk.js"
+  },
+  {
+    "revision": "8ae279e0396a82bcb324f2e1f01716e9",
+    "url": "/static/js/2.9a5d554f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe52c5ef97c7fd1c23d8",
+    "url": "/static/js/main.7d5204d7.chunk.js"
+  },
+  {
+    "revision": "77b49933e7fb325e77ae",
+    "url": "/static/js/runtime-main.8c8a8e8a.js"
+  },
+  {
+    "revision": "82a9b911484d37a2a61222d0eb0656ac",
+    "url": "/static/media/johndoe.82a9b911.svg"
+  },
+  {
+    "revision": "8ab4fec3cb0f14f1c09bcdaf695db372",
+    "url": "/static/media/mk.8ab4fec3.svg"
+  },
+  {
+    "revision": "8deb430b89d5385e885646ebe38e94a5",
+    "url": "/static/media/sarahedo.8deb430b.svg"
+  },
+  {
+    "revision": "3a3215e6c42bd6a16d9fc669f820b787",
+    "url": "/static/media/tylermcginnis.3a3215e6.svg"
+  }
+]);
